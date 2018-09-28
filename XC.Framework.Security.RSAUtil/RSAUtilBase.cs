c770fedfdb92c3982d70b8f4bc74cc27f3bea845
dancnc <273230766@qq.com>
2018-09-28 11:55:55 +0800

    Update RSAUtilBase.cs

--- XC.Framework.Security.RSAUtil/RSAUtilBase.cs
+++ XC.Framework.Security.RSAUtil/RSAUtilBase.cs
@@ -1,88 +1,136 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace XC.Framework.Security.RSAUtil
+namespace XC.RSAUtil
 {
-	public abstract class RSAUtilBase
-	{
-		public RSA PrivateRsa;
-		public RSA PublicRsa;
-		public Encoding DataEncoding;
+    public abstract class RSAUtilBase
+    {
+        public RSA PrivateRsa;
+        public RSA PublicRsa;
+        public Encoding DataEncoding;
 
-		/// <summary>
-		/// RSA public key encryption
-		/// </summary>
-		/// <param name="data">Need to encrypt data</param>
-		/// <param name="padding">Padding algorithm</param>
-		/// <returns></returns>
-		public string Encrypt(string data, RSAEncryptionPadding padding)
-		{
-			if (PublicRsa == null)
-			{
-				throw new ArgumentException("public key can not null");
-			}
-			byte[] dataBytes = DataEncoding.GetBytes(data);
-			var resBytes = PublicRsa.Encrypt(dataBytes, padding);
-			return Convert.ToBase64String(resBytes);
-		}
+        /// <summary>
+        /// RSA public key encryption
+        /// </summary>
+        /// <param name="data">Need to encrypt data</param>
+        /// <param name="padding">Padding algorithm</param>
+        /// <returns></returns>
+        public string Encrypt(string data, RSAEncryptionPadding padding)
+        {
+            if (PublicRsa == null)
+            {
+                throw new ArgumentException("public key can not null");
+            }
+            byte[] dataBytes = DataEncoding.GetBytes(data);
 
-		/// <summary>
-		/// RSA private key is decrypted
-		/// </summary>
-		/// <param name="data">Need to decrypt the data</param>
-		/// <param name="padding">Padding algorithm</param>
-		/// <returns></returns>
-		public string Decrypt(string data, RSAEncryptionPadding padding)
-		{
-			if (PrivateRsa == null)
-			{
-				throw new ArgumentException("private key can not null");
-			}
-			byte[] dataBytes = Convert.FromBase64String(data);
-			var resBytes = PrivateRsa.Decrypt(dataBytes, padding);
-			return DataEncoding.GetString(resBytes);
-		}
+            int bufferSize = (PublicRsa.KeySize / 8) - 11;//单块最大长度
+            var buffer = new byte[bufferSize];
+            using (MemoryStream inputStream = new MemoryStream(dataBytes),
+                 outputStream = new MemoryStream())
+            {
+                while (true)
+                { //分段加密
+                    int readSize = inputStream.Read(buffer, 0, bufferSize);
+                    if (readSize <= 0)
+                    {
+                        break;
+                    }
+                    var temp = new byte[readSize];
+                    Array.Copy(buffer, 0, temp, 0, readSize);
+                    var encryptedBytes = PublicRsa.Encrypt(temp, padding);
+                    outputStream.Write(encryptedBytes, 0, encryptedBytes.Length);
+                }
+                return Convert.ToBase64String(outputStream.ToArray());//转化为字节流方便传输
+            }
+        }
 
-		/// <summary>
-		/// Use private key for data signing
-		/// </summary>
-		/// <param name="data">Need to sign data</param>
-		/// <param name="hashAlgorithmName">Signed hash algorithm name</param>
-		/// <param name="padding">Signature padding algorithm</param>
-		/// <returns></returns>
-		public string SignData(string data, HashAlgorithmName hashAlgorithmName, RSASignaturePadding padding)
-		{
-			if (PrivateRsa == null)
-			{
-				throw new ArgumentException("private key can not null");
-			}
-			var dataBytes = DataEncoding.GetBytes(data);
-			var res = PrivateRsa.SignData(dataBytes, hashAlgorithmName, padding);
-			return Convert.ToBase64String(res);
-		}
+        /// <summary>
+        /// RSA private key is decrypted
+        /// </summary>
+        /// <param name="data">Need to decrypt the data</param>
+        /// <param name="padding">Padding algorithm</param>
+        /// <returns></returns>
+        public string Decrypt(string data, RSAEncryptionPadding padding)
+        {
+            if (PrivateRsa == null)
+            {
+                throw new ArgumentException("private key can not null");
+            }
+            byte[] dataBytes = Convert.FromBase64String(data);
+            int bufferSize = PrivateRsa.KeySize / 8;
+            var buffer = new byte[bufferSize];
+            using (MemoryStream inputStream = new MemoryStream(dataBytes),
+                 outputStream = new MemoryStream())
+            {
+                while (true)
+                {
+                    int readSize = inputStream.Read(buffer, 0, bufferSize);
+                    if (readSize <= 0)
+                    {
+                        break;
+                    }
+                    var temp = new byte[readSize];
+                    Array.Copy(buffer, 0, temp, 0, readSize);
+                    var rawBytes = PrivateRsa.Decrypt(temp, padding);
+                    outputStream.Write(rawBytes, 0, rawBytes.Length);
+                }
+                return Encoding.UTF8.GetString(outputStream.ToArray());
+            }
+        }
 
-		/// <summary>
-		/// Use public key to verify data signature
-		/// </summary>
-		/// <param name="data">Need to verify the signature data</param>
-		/// <param name="sign">sign</param>
-		/// <param name="hashAlgorithmName">Signed hash algorithm name</param>
-		/// <param name="padding">Signature padding algorithm</param>
-		/// <returns></returns>
-		public bool VerifyData(string data, string sign, HashAlgorithmName hashAlgorithmName, RSASignaturePadding padding)
-		{
-			if (PublicRsa == null)
-			{
-				throw new ArgumentException("public key can not null");
-			}
-			var dataBytes = DataEncoding.GetBytes(data);
-			var signBytes = Convert.FromBase64String(sign);
-			var res = PublicRsa.VerifyData(dataBytes, signBytes, hashAlgorithmName, padding);
-			return res;
-		}
+        /// <summary>
+        /// Use private key for data signing
+        /// </summary>
+        /// <param name="data">Need to sign data</param>
+        /// <param name="hashAlgorithmName">Signed hash algorithm name</param>
+        /// <param name="padding">Signature padding algorithm</param>
+        /// <returns></returns>
+        public string SignData(string data, HashAlgorithmName hashAlgorithmName, RSASignaturePadding padding)
+        {
+            var res = SignDataGetBytes(data, hashAlgorithmName, padding);
+            return Convert.ToBase64String(res);
+        }
 
-		protected abstract RSAParameters CreateRsapFromPrivateKey(string privateKey);
-		protected abstract RSAParameters CreateRsapFromPublicKey(string publicKey);
-	}
+        /// <summary>
+        /// Use private key for data signing
+        /// </summary>
+        /// <param name="data">Need to sign data</param>
+        /// <param name="hashAlgorithmName">Signed hash algorithm name</param>
+        /// <param name="padding">Signature padding algorithm</param>
+        /// <returns>Sign bytes</returns>
+        public byte[] SignDataGetBytes(string data, HashAlgorithmName hashAlgorithmName, RSASignaturePadding padding)
+        {
+            if (PrivateRsa == null)
+            {
+                throw new ArgumentException("private key can not null");
+            }
+            var dataBytes = DataEncoding.GetBytes(data);
+            return PrivateRsa.SignData(dataBytes, hashAlgorithmName, padding);
+        }
+
+        /// <summary>
+        /// Use public key to verify data signature
+        /// </summary>
+        /// <param name="data">Need to verify the signature data</param>
+        /// <param name="sign">sign</param>
+        /// <param name="hashAlgorithmName">Signed hash algorithm name</param>
+        /// <param name="padding">Signature padding algorithm</param>
+        /// <returns></returns>
+        public bool VerifyData(string data, string sign, HashAlgorithmName hashAlgorithmName, RSASignaturePadding padding)
+        {
+            if (PublicRsa == null)
+            {
+                throw new ArgumentException("public key can not null");
+            }
+            var dataBytes = DataEncoding.GetBytes(data);
+            var signBytes = Convert.FromBase64String(sign);
+            var res = PublicRsa.VerifyData(dataBytes, signBytes, hashAlgorithmName, padding);
+            return res;
+        }
+
+        protected abstract RSAParameters CreateRsapFromPrivateKey(string privateKey);
+        protected abstract RSAParameters CreateRsapFromPublicKey(string publicKey);
+    }
 }
