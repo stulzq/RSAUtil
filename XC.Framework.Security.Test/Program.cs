2c04aec221fca3c8168248bb9515bc3f360d7430
stulzq <stulzq@qq.com>
2019-02-21 11:14:51 +0800

    [Add] key convert sample code

--- XC.Framework.Security.Test/Program.cs
+++ XC.Framework.Security.Test/Program.cs
@@ -10,13 +10,18 @@ namespace XC.Framework.Security.Test
     {
         static void Main(string[] args)
         {
-	        var keyList = RsaKeyGenerator.XmlKey(2048);
-	        var privateKey = keyList[0];
-	        var publicKey = keyList[1];
 
 			Console.WriteLine(RsaKeyGenerator.XmlKey(2048)[0]);
             Console.WriteLine(RsaKeyGenerator.Pkcs1Key(2048,true)[0]);
             Console.WriteLine(RsaKeyGenerator.Pkcs8Key(2048,true)[0]);
+
+
+            Console.WriteLine("Key Convert:");
+            var keyList = RsaKeyGenerator.Pkcs1Key(2048, false);
+            var privateKey = keyList[0];
+            var publicKey = keyList[1];
+            Console.WriteLine("public key pkcs1->xml:");
+            Console.WriteLine(RsaKeyConvert.PublicKeyPemToXml(publicKey));
 
             Console.ReadKey();
         }
